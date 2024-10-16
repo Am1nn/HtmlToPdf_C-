f8ca7a583c18fac7db7b1699044751ed0efe332b
Amin Bennayev <129307929+Am1nn@users.noreply.github.com>
2024-10-16 21:17:24 +0400

    Add project files.

---
+++ TestPdf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NReco.PdfGenerator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var htmlFilePath = @"C:\Users\Amin\Desktop\twstt\index.html";
+
+        var htmlContent = File.ReadAllText(htmlFilePath);
+
+        var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
+        var pdfBytes = htmlToPdf.GeneratePdf(htmlContent);
+
+        var pdfFilePath = @"output.pdf";
+        File.WriteAllBytes(pdfFilePath, pdfBytes);
+
+    }
+}
